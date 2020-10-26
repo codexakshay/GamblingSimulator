a510164e481784731b6851360290e0a29390d5bb
codexakshay <codexakshay@gmail.com>
2020-10-26 09:41:47 +0530

    GamblingSimulation -> UC2_GamblingWinLose : Gambler is making bet lose : -1 win : +1

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,38 +1,26 @@
 package com.codex.sim;
 
 import java.util.Random;
-
-public class GamblingSimulation {
-	static int gambler = 100;
-	int win = 1;
-	int lose = 0;
-	Random rand = new Random();
-	public void 	bet ()
-	{
-		gambler = gambler - 1;
-		int bet = rand.nextInt() % 2;
-		System.out.print(bet);
 import java.math.*;
+
 public class GamblingSimulation {
 	static int gambler = 100;
 	public static final int win = 1;
 	public static final int lose = 0;
-	public void 	bet ()
-	{
+
+	public void bet() {
 		gambler = gambler - 1;
-		int bet =  (int)Math.round(Math.random());
-		if(bet == win)
-		{
-			gambler = gambler + 2 ;
+		int bet = (int) Math.round(Math.random());
+		if (bet == win) {
+			gambler = gambler + 2;
 			System.out.println("Gambler won the bet");
-			System.out.println("Gambler Money : "+gambler+"$");
-		}
-		else
-		{
+			System.out.println("Gambler Money : " + gambler + "$");
+		} else {
 			System.out.println("Gambler lost the bet");
-			System.out.println("Gambler Money : "+gambler+"$");
+			System.out.println("Gambler Money : " + gambler + "$");
 		}
 	}
+
 	public static void main(String[] args) {
 		GamblingSimulation gamble = new GamblingSimulation();
 		gamble.bet();
