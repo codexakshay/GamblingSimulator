a862f13a08c65fec0f699e2fa341880388c39796
codexakshay <codexakshay@gmail.com>
2020-10-26 10:00:22 +0530

    GamblingSimulation -> UC3_GamblingResult : Gambler is making bet lose : -50% win : +50%

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -7,22 +7,30 @@ public class GamblingSimulation {
 	static int gambler = 100;
 	public static final int win = 1;
 	public static final int lose = 0;
-
+	public void InitialMoney()
+	{
+		System.out.println("Gambler Money : " + gambler + "$");
+	}
 	public void bet() {
 		gambler = gambler - 1;
 		int bet = (int) Math.round(Math.random());
 		if (bet == win) {
-			gambler = gambler + 2;
+			gambler = gambler + (gambler * 1/2);
 			System.out.println("Gambler won the bet");
 			System.out.println("Gambler Money : " + gambler + "$");
 		} else {
+			gambler = gambler + (gambler * 1/2);
 			System.out.println("Gambler lost the bet");
 			System.out.println("Gambler Money : " + gambler + "$");
 		}
 	}
-
+	public void Gambling()
+	{
+		InitialMoney();
+		bet();
+	}
 	public static void main(String[] args) {
 		GamblingSimulation gamble = new GamblingSimulation();
-		gamble.bet();
+		gamble.Gambling();
 	}
 }
