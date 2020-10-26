217713d7a824f62cb318cb0c2ffda2946405b473
codexakshay <codexakshay@gmail.com>
2020-10-26 11:12:02 +0530

    Added : DAYS (1 to 20) and Printing the Total wins and loses amount

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -6,28 +6,53 @@ import java.math.*;
 public class GamblingSimulation {
 	static int gambler = 100;
 	public static final int WIN = 1;
-	public void InitialMoney()
-	{
+	static int days = 1;
+	static int totalWon=0,totalLost=0; 
+
+	public void InitialMoney() {
 		System.out.println("Gambler Money : " + gambler + "$");
 	}
+
 	public void bet() {
 		gambler = gambler - 1;
+		System.out.print("\n\tDAY : " + days);
 		int bet = (int) Math.round(Math.random());
-		if (bet == WIN) {
-			gambler = gambler + (gambler * 1/2);
-			System.out.println("Gambler won the bet");
-			System.out.println("Gambler Money : " + gambler + "$");
+		int reward = gambler * 1 / 2;
+		if (gambler > 0) {
+			if (bet == WIN) {
+				gambler = gambler + reward;
+				System.out.println("\nGambler won the bet");
+				System.out.println("Gambler Money : " + gambler + "$");
+				totalWon = totalLost + reward;
+			} else {
+				gambler = gambler - reward;
+				System.out.println("\nGambler lost the bet");
+				System.out.println("Gambler Money : " + gambler + "$");
+				totalLost = totalLost + reward;
+			}
+			System.out.println("DAY : " + days + "\nReward : " + reward + "$");
+			days = days + 1;
+			System.out.println("Total Won : " + totalWon + "$");
+			System.out.println("Total Lost : " + totalLost + "$");
 		} else {
-			gambler = gambler - (gambler * 1/2);
-			System.out.println("Gambler lost the bet");
-			System.out.println("Gambler Money : " + gambler + "$");
+			System.out.println("\nHAHA... YOU ARE BROKE");
+			System.out.println("Total Won : " + totalWon + "$");
+			System.out.println("Total Lost : " + totalLost + "$");
+			System.exit(0);
 		}
 	}
-	public void Gambling()
-	{
-		InitialMoney();
-		bet();
+
+	public void betDays() {
+		do {
+			bet();
+		} while (days <= 20);
 	}
+
+	public void Gambling() {
+		InitialMoney();
+		betDays();
+	}
+
 	public static void main(String[] args) {
 		GamblingSimulation gamble = new GamblingSimulation();
 		gamble.Gambling();
