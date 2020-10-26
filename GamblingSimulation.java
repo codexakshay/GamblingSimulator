ecf8b5f5928994e9bcd57545bc56839c1ffdc024
codexakshay <codexakshay@gmail.com>
2020-10-26 11:26:13 +0530

    Added : Printing the Total Times won and Times lost

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -7,7 +7,8 @@ public class GamblingSimulation {
 	static int gambler = 100;
 	public static final int WIN = 1;
 	static int days = 1;
-	static int totalWon=0,totalLost=0; 
+	static int totalWon = 0, totalLost = 0;
+	static int winCount = 0, LoseCount = 0;
 
 	public void InitialMoney() {
 		System.out.println("Gambler Money : " + gambler + "$");
@@ -21,25 +22,38 @@ public class GamblingSimulation {
 		if (gambler > 0) {
 			if (bet == WIN) {
 				gambler = gambler + reward;
-				System.out.println("\nGambler won the bet");
+				System.out.println("\n\nGambler won the bet");
 				System.out.println("Gambler Money : " + gambler + "$");
 				totalWon = totalWon + reward;
-				
+				winCount = winCount + 1;
+
 			} else {
 				gambler = gambler - reward;
-				System.out.println("\nGambler lost the bet");
-				System.out.println("Gambler Money : " + gambler + "$");
+				System.out.println("\n\nGambler lost the bet");
+				System.out.println("\nGambler Money : " + gambler + "$");
 				totalLost = totalLost + reward;
+				LoseCount = LoseCount + 1;
 			}
 			System.out.println("DAY : " + days + "\nReward : " + reward + "$");
 			days = days + 1;
-			System.out.println("Total Won : " + totalWon + "$");
-			System.out.println("Total Lost : " + totalLost + "$");
+			
+			System.out.println("\nTotal Amount Won : " + totalWon + "$");
+			System.out.println("Total Amount Lost : " + totalLost + "$");
+
+			System.out.println("\nTotal Win : " + winCount + " Times");
+			System.out.println("Total Lost Times : " + LoseCount + " Times");
 			
 		} else {
 			System.out.println("\nHAHA... YOU ARE BROKE");
-			System.out.println("Total Won : " + totalWon + "$");
-			System.out.println("Total Lost : " + totalLost + "$");
+			
+			System.out.println("\nGambler Money : " + gambler + "$");
+			
+			System.out.println("\nTotal Amount Won : " + totalWon + "$");
+			System.out.println("Total Amount Lost : " + totalLost + "$");
+			
+			System.out.println("\nTotal Win : " + winCount + " Times");
+			System.out.println("Total Lost : " + LoseCount + " Times");
+			
 			System.exit(0);
 		}
 	}
