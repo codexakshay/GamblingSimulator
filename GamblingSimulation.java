fe02bd16f42fec80d0c24ffd1921186c222d9abc
codexakshay <codexakshay@gmail.com>
2020-10-28 15:45:12 +0530

    Added : Play Again for Gambler if he/she Survives 20 Gambling Days

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,12 +1,14 @@
 package com.codex.sim;
 
+import java.util.Scanner;
+
 public class GamblingSimulation {
 	static int gambler = 100;
 	public static final int WIN = 1;
 	static int days = 1;
 	static int totalWon = 0, totalLost = 0;
 	static int winCount = 0, LoseCount = 0;
-	static int maxReward = 0, luckyDay = 0;
+	static int month = 1, maxReward = 0, luckyDay = 0;
 
 	public void InitialMoney() {
 		System.out.println("Gambler Money : " + gambler + "$");
@@ -14,7 +16,7 @@ public class GamblingSimulation {
 
 	public void bet() {
 		gambler = gambler - 1;
-		System.out.print("\n\tDAY : " + days);
+		System.out.print("\n*MONTH : " + month + " DAY : " + days + "*");
 		int bet = (int) Math.round(Math.random());
 		int reward = gambler * 1 / 2;
 		if (gambler > 0) {
@@ -57,18 +59,45 @@ public class GamblingSimulation {
 			System.out.println("Total Lost : " + LoseCount + " Times");
 
 			System.out.println("\nMax Reward : " + maxReward + "$");
-			System.out.println("Luckiest Day : " + luckyDay);
+			System.out.println("Luckiest Day : MONTH : " + month + " DAY : " + luckyDay);
 
 			System.exit(0);
 		}
 		System.out.println("\nMax Reward : " + maxReward + "$");
-		System.out.println("Luckiest Day : " + luckyDay);
+		System.out.println("Luckiest Day : MONTH : " + month + " DAY : " + luckyDay);
 	}
 
 	public void betDays() {
 		do {
 			bet();
-		} while (days <= 20);
+			playAgain();
+		} while (days <= 21);
+
+	}
+
+	public void playAgain() {
+		if (days == 21) {
+			month += 1;
+			System.out.println("\n\nWant to play one more month ? ");
+			System.out.println("1. Yes ");
+			System.out.println("2. No");
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Your choise : ");
+			int choice = scan.nextInt();
+			switch (choice) {
+			case 1:
+				days = 1;
+				System.out.println("Don't Cry after Getting Broke  ... haha");
+				betDays();
+				break;
+			case 2:
+				System.out.println("Too Scared to Lose , huh ? ... haha");
+				System.exit(0);
+			default:
+				System.out.println("Invalid Choice ... Choose Wisely!!!");
+				playAgain();
+			}
+		}
 	}
 
 	public void Gambling() {
